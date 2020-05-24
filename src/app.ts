aa80b81ffae6843bb45b34da344835c5e89c7fda
Boldy97 <robin.de.bruin@hotmail.com>
2020-05-24 16:22:58 +0200

    Dockerize - [ch109]

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,13 @@ import Game from './game';
 import Connection from './networking/connection';
 import { InviteMessage } from './networking/message';
 
-const URL = 'https://localhost:8080/socket';
+let URL = 'https://localhost:8080/socket';
 //const URL = 'https://api.wartemis.com/socket';
+
+console.log(process.env.WARTEMIS_ENV);
+if(process.env.WARTEMIS_ENV === 'BUILD') {
+  URL = 'https://pw-backend/socket';
+}
 
 new Connection(URL)
   .registerHandler('invite', handleInviteMessage);
